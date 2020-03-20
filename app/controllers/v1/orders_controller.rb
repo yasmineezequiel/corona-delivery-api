65b829cad43ebe52a363f20c5e6786b695bc5c88
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-03-20 16:03:00 +0100

    Adding create action into orders controller and def params and its attributes.

---
+++ app/controllers/v1/orders_controller.rb
@@ -0,0 +1,16 @@
+class V1::OrdersController < ApplicationController
+    def create
+      order = Order.create(order_params)
+      if order.persisted?
+        render json: { message: 'The order was successfully created.' }, status: 201
+      else
+        render json: { error_message: 'Unable to create order' }, status: 422
+      end
+    end
+  
+    private
+  
+    def order_params
+      params.require(:order).permit(:accessible_item, :email, :location, :phone)
+    end
+  end
